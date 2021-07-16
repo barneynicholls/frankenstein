1718c1386f103f7fe50f0ef8293c0baf10c47c93
Barney Nicholls <Barnaby.Nicholls@PAYROC.COM>
2021-07-16 08:13:34 +0000

    first check in

---
+++ Frankenstein/Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Frankenstein
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timerInterval = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keysListBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ok = new System.Windows.Forms.Button();
+            this.showBalloon = new System.Windows.Forms.CheckBox();
+            this.moveMouse = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.timerInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.trayIcon.BalloonTipText = "Sending X key every X seconds";
+            this.trayIcon.BalloonTipTitle = "It\'s alive!!!";
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Frankenstein\'s Monster";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseClick);
+            // 
+            // timerInterval
+            // 
+            this.timerInterval.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerInterval.Location = new System.Drawing.Point(53, 55);
+            this.timerInterval.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.timerInterval.Name = "timerInterval";
+            this.timerInterval.Size = new System.Drawing.Size(260, 20);
+            this.timerInterval.TabIndex = 0;
+            this.timerInterval.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.timerInterval.ValueChanged += new System.EventHandler(this.timerInterval_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Every";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Press";
+            // 
+            // keysListBox
+            // 
+            this.keysListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.keysListBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.keysListBox.FormattingEnabled = true;
+            this.keysListBox.Location = new System.Drawing.Point(53, 19);
+            this.keysListBox.Name = "keysListBox";
+            this.keysListBox.Size = new System.Drawing.Size(260, 21);
+            this.keysListBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "key";
+            // 
+            // ok
+            // 
+            this.ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok.Location = new System.Drawing.Point(291, 150);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(75, 23);
+            this.ok.TabIndex = 6;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // showBalloon
+            // 
+            this.showBalloon.Location = new System.Drawing.Point(53, 120);
+            this.showBalloon.Name = "showBalloon";
+            this.showBalloon.Size = new System.Drawing.Size(260, 24);
+            this.showBalloon.TabIndex = 7;
+            this.showBalloon.Text = "Show notification at startup";
+            this.showBalloon.UseVisualStyleBackColor = true;
+            // 
+            // moveMouse
+            // 
+            this.moveMouse.AutoSize = true;
+            this.moveMouse.Location = new System.Drawing.Point(53, 97);
+            this.moveMouse.Name = "moveMouse";
+            this.moveMouse.Size = new System.Drawing.Size(88, 17);
+            this.moveMouse.TabIndex = 8;
+            this.moveMouse.Text = "Move Mouse";
+            this.moveMouse.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 185);
+            this.Controls.Add(this.moveMouse);
+            this.Controls.Add(this.showBalloon);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.keysListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timerInterval);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frankenstein";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timerInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.CheckBox showBalloon;
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ok;
+
+        private System.Windows.Forms.ComboBox keysListBox;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.NumericUpDown timerInterval;
+        private System.Windows.Forms.Label label1;
+
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox moveMouse;
+    }
+}
---
+++ Frankenstein/Main.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Frankenstein.Properties;
+
+namespace Frankenstein
+{
+    public partial class Main : Form
+    {
+        private readonly Timer _timer = new Timer();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void trayIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            foreach (var key in Enum.GetValues(typeof(Keys)))
+            {
+                keysListBox.Items.Add(key);
+            }
+
+            var keyPress = Enum.Parse(typeof(Keys), Settings.Default.KeyPress);
+
+            keysListBox.SelectedItem = keyPress;
+            
+            showBalloon.Checked = Settings.Default.ShowTipAtStart;
+
+            if (showBalloon.Checked)
+            {
+                trayIcon.BalloonTipText = string.Format(
+                    "Pressing '{0}' every {1} second{2}",
+                    keysListBox.SelectedItem,
+                    timerInterval.Value,
+                    timerInterval.Value == 1 ? string.Empty : "s");
+
+                trayIcon.ShowBalloonTip(500);
+            }
+
+            moveMouse.Checked = Settings.Default.MoveMouse;
+
+            var interval = Settings.Default.Interval;
+
+            timerInterval.Value = interval;
+
+            SetTimer();
+
+            TimerEventProcessor(null, null);
+        }
+
+        private void MoveMousePosition()
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            Cursor.Position = new Point(Cursor.Position.X - 1, Cursor.Position.Y - 1);
+
+            Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y + 1);
+        }
+
+        private void SetTimer()
+        {
+            _timer.Stop();
+
+            _timer.Tick += TimerEventProcessor;
+
+            _timer.Interval = Convert.ToInt32(timerInterval.Value) * 1000;
+
+            _timer.Tick += TimerEventProcessor;
+
+            _timer.Start();
+        }
+
+        private void TimerEventProcessor(object sender, EventArgs e)
+        {
+            try
+            {
+                var key = string.Format("{{{0}}}", keysListBox.SelectedItem);
+
+                SendKeys.Send(key);
+
+                if (moveMouse.Checked)
+                    MoveMousePosition();
+
+                Text = string.Format(
+                    "Frankenstein pressed {0} @ {1}",
+                    keysListBox.SelectedItem,
+                    DateTime.Now.ToLongTimeString());
+            }
+            catch
+            {
+                Text = string.Format(
+                    "Frankenstein Failed @ {0}",
+                    DateTime.Now.ToLongTimeString());
+            }
+
+            trayIcon.Text = Text;
+        }
+
+        private void timerInterval_ValueChanged(object sender, EventArgs e)
+        {
+            SetTimer();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _timer.Stop();
+            _timer.Tick -= TimerEventProcessor;
+
+            SaveSettings();
+        }
+
+        private void SaveSettings()
+        {
+            Settings.Default.ShowTipAtStart = showBalloon.Checked;
+            Settings.Default.Interval = Convert.ToInt32(timerInterval.Value);
+            Settings.Default.KeyPress = keysListBox.SelectedItem.ToString();
+            Settings.Default.MoveMouse = moveMouse.Checked;
+            Settings.Default.Save();
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
